55cc3346a1b75bd096fb8354d040055d75969886
star0chris <56282175+star0chris@users.noreply.github.com>
2019-12-05 20:33:55 -0700

    Updated version

--- src/formatTextFile/Formatter.java
+++ src/formatTextFile/Formatter.java
@@ -26,9 +26,8 @@ import java.io.PrintStream;
 
 public class Formatter {
 	protected int total, textAlign;
-	protected boolean doubleSpaced, block, indent, twoColumn, title;
-	protected String currentString, spillOver, inputFileName, outputFileName, indentLength, extra;
-	protected BufferedReader reader;
+	protected boolean doubleSpaced, block, indent, twoColumn, title, flagged, checkTwoColumn, checkDoubleS, checkBlock;
+	protected String currentString, spillOver, inputFileName, outputFileName, indentLength, extra, testString;
 
 	public Formatter(String inputFile, String outputFile) {
 		indentLength = "     "; // 5 spaces long
@@ -47,25 +46,30 @@ public class Formatter {
 	}
 
 	public void readFile() {
+		BufferedReader reader;
 		try {
 			reader = new BufferedReader(new FileReader(inputFileName));
+			PrintStream out = new PrintStream(new FileOutputStream(outputFileName));
+			System.setOut(out);
 			String line = "";
 			while (line != null) {
 
 				// read next line
 				line = reader.readLine();
-				if (line.length() == 2) {
+				if (line == null) {
+					printRemainder();
+				} else if (line.length() == 2) {
 					checkFlags(line);
-				} 
-				else {
+				} else {
 					if (title) {
 						printTitle(line);
-					}
-					if (twoColumn) {
+					} else if (twoColumn) {
 						printTwoColumn(line);
-					} 
-					else {
+					} else {
 						printSingleLine(line);
+					}
+					if (doubleSpaced) {
+						System.out.print("\n");
 					}
 				}
 
@@ -74,9 +78,6 @@ public class Formatter {
 				}
 			}
 			reader.close();
-			printSingleLine(spillOver);
-			PrintStream out = new PrintStream(new FileOutputStream(outputFileName));
-			System.setOut(out);
 
 			out.close();
 		} catch (IOException e) {
@@ -84,8 +85,35 @@ public class Formatter {
 		}
 	}
 
+	public void printRemainder() {
+
+		String temp = testString.toLowerCase();
+		while (extra.length() > 0) {
+			if (checkTwoColumn || twoColumn) {
+				printTwoColumn(extra);
+				if (doubleSpaced && testString.equals("-1")) {
+					System.out.print("\n");
+				}
+			} else {
+				printSingleLine(extra);
+				if (doubleSpaced && temp.equals("-b")) {
+					System.out.print("\n");
+				}
+			}
+			if (checkDoubleS) {
+				System.out.print("\n");
+			}
+		}
+		checkTwoColumn = false;
+		checkDoubleS = false;
+		checkBlock = false;
+		return;
+	}
+
 	public void checkFlags(String line) {
 		String temp = line.toLowerCase();
+		testString = line;
+		flagged = true;
 		if (temp.equals("-r")) {
 			textAlign = 2; // right flag
 		} else if (temp.equals("-c")) {
@@ -99,30 +127,43 @@ public class Formatter {
 			block = false;
 		} else if (temp.equals("-d")) {
 			doubleSpaced = true;
+			checkDoubleS = false;
 		} else if (temp.equals("-s")) {
 			doubleSpaced = false;
+			checkDoubleS = true;
 		} else if (temp.equals("-i")) {
 			indent = true;
 		} else if (temp.equals("-b")) {
 			block = true;
+			checkBlock = true;
 		} else if (temp.equals("-e")) {
 			if (twoColumn = false) {
 				System.out.print("\n");
 			}
 		} else if (temp.equals("-2")) {
 			twoColumn = true;
+			checkTwoColumn = true;
 		} else if (temp.equals("-1")) {
 			twoColumn = false;
+			checkTwoColumn = true;
 		} else {
+			flagged = false;
 			printSingleLine(line);
+		}
+		if (flagged) {
+			flagged = false;
+			printRemainder();
 		}
 	}
 
 	public void printTwoColumn(String line) {
 		spillOver = line;
-		if (extra.length() > 0) {
-			spillOver = extra + spillOver;
-			extra = "";
+		String extra2 = "";
+		if (spillOver != extra) {
+			if (extra.length() > 0) {
+				spillOver = extra + spillOver;
+				extra = "";
+			}
 		}
 		if (spillOver.length() >= 70) {
 			currentString = spillOver.substring(0, 71);
@@ -132,10 +173,16 @@ public class Formatter {
 		}
 		if (currentString.length() < 36) {
 			System.out.print(currentString + indentLength + indentLength + "\n");
+			if (extra.equals(currentString)) {
+				extra = "";
+			}
 		} else {
-			String extra2 = currentString.substring(0, 36);
+			extra2 = currentString.substring(0, 36);
 			currentString = currentString.substring(36);
 			System.out.print(extra2 + indentLength + indentLength + currentString + "\n");
+			if (extra.equals(extra2 + currentString)) {
+				extra = "";
+			}
 		}
 		return;
 	}
@@ -147,20 +194,23 @@ public class Formatter {
 			temp += " ";
 		}
 		temp = temp + line + temp;
-		System.out.print(temp + "\n");
+		System.out.println(temp);
 	}
 
 	public void printSingleLine(String line) {
 		spillOver = line;
-		if (extra.length() > 0) {
-			spillOver = extra + spillOver;
-			extra = "";
+		if (spillOver != extra) {
+			if (extra.length() > 0) {
+				spillOver = extra + spillOver;
+				extra = "";
+			}
 		}
 		if (indent) {
 			spillOver = indentLength + spillOver;
 			indent = false;
-		} else if (block) {
+		} else if (block && !checkBlock) {
 			spillOver = indentLength + indentLength + spillOver;
+			testString = extra;
 		}
 		if (spillOver.length() >= 80) {
 			currentString = spillOver.substring(0, 81);
@@ -173,12 +223,22 @@ public class Formatter {
 					temp += " ";
 				}
 				currentString = temp + currentString;
+				if (currentString.equals(temp + extra) || testString.equals(temp + extra)) {
+					extra = "";
+				}
 			} else if (textAlign == 1) {
 				String temp = "";
 				while (2 * temp.length() + currentString.length() < 80) {
 					temp += " ";
 				}
 				currentString = temp + currentString + temp;
+				if (currentString.equals(temp + extra + temp) || testString.equals(temp + extra + temp)) {
+					extra = "";
+				}
+			} else {
+				if (currentString.equals(extra) || testString.equals(extra)) {
+					extra = "";
+				}
 			}
 
 		}
@@ -198,15 +258,16 @@ public class Formatter {
 			errorMessage += "<font color=red>Error: The input and output file names cannot be the same.</font><br>";
 		}
 		// Check to see if the output file has the .txt suffix
-		if (outputFileName.indexOf(".txt", outputFileName.length() -4) == -1) {
+		if (outputFileName.indexOf(".txt", outputFileName.length() - 4) == -1) {
 			outputFileName += ".txt";
 			errorMessage += "<font color=red>Error: Output file is not specified as type txt.</font><br>";
 		}
 		// Check to see if the input file has the .txt suffix
 		if (inputFileName.indexOf(".txt", inputFileName.length() - 4) == -1) {
 			errorMessage += "<font color=red>Error: Input file is not specified as type txt.</font><br>";
-		} 		
-		// None of the conditions above are true. Return "<html></html>", meaning all is clear
+		}
+		// None of the conditions above are true. Return "<html></html>", meaning all is
+		// clear
 		errorMessage += "</html>";
 		return errorMessage;
 	}
